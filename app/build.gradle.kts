8a25e8f7fe62662bba0e72b3addb3a07b86d45d4
spacekiku <spacekiku@gmail.com>
2025-11-30 12:11:00 +0900

    chore: remove commons-compress; update compose-bom to 2024.12.01 for security fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,6 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.okhttp)
     implementation("com.squareup.okhttp3:okhttp:3.12.0")
-    implementation("org.apache.commons:commons-compress:1.21")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
