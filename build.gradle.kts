4ec07d35cb12db41ee1c0d456935bab9d8516d4a
spacekiku <spacekiku@gmail.com>
2025-11-30 13:51:12 +0900

    ci: submit app+library dependency graphs; build: add Versions Plugin for updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,11 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    // For dependency update reports
+    id("com.github.ben-manes.versions") version "0.51.0" apply false
+}
+
+// Enable the versions plugin at the root to run reports
+subprojects {
+    plugins.apply("com.github.ben-manes.versions")
 }
